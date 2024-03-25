2c91a47d0e40cd74a9cb7843fdb3ddf3d8d44473
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2024-03-26 05:27:28 +0900

    プロジェクト ファイルを追加します。

---
+++ C++MagicBook/Chapter1-class/class.cpp
@@ -0,0 +1,45 @@
+//class.cpp
+#include "class.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+//メンバ関数の定義は全て、"クラス名::関数名(引数)"となる
+
+//コンストラクタの定義
+POS::POS()
+{
+	//thisポインタは自身のメンバを指定している
+	this->x = 0;
+	this->y = 0;
+}
+
+//引数ありコンストラクタの定義
+POS::POS(int x, int y)
+{
+	//thisポインタを使用することで変数名と引数名が同じでも大丈夫
+	this->x = x;
+	this->y = y;
+}
+
+//デストラクタの定義
+POS::~POS()
+{
+	//今のコードでは何も書く必要がない
+}
+
+//メンバ関数の定義
+int POS::GetPosX(void) const
+{
+	return this->x;
+}
+
+int POS::GetPosY(void) const
+{
+	return this->y;
+}
+
+void POS::SetPos(int x, int y)
+{
+	this->x = x;
+	this->y = y;
+}
