6c59fa31473121f45d9df3c62c7150272ff55898
ryuu3160(MainPC) <principe2580613@yahoo.co.jp>
2024-03-26 08:24:20 +0900

    クラスの説明完成

---
+++ C++MagicBook/CodeTest.cpp
@@ -0,0 +1,19 @@
+//CodeTest.cpp
+//それぞれのチャプターのプログラムを実行する場所
+#include <stdio.h>
+#include <stdlib.h>
+
+//===== SAMPLE INCLUDE =====
+#include "Chapter1-class/class.h"
+
+int main(void)
+{
+	//----- CLASS -----
+	POS a;
+	POS b(20, 30);
+	a.SetPos(10, 15);
+
+	printf("a.X:%d a.Y:%d\nb.X:%d b.Y:%d\n", a.GetPosX(), a.GetPosY(), b.GetPosX(), b.GetPosY());
+
+	return 0;
+}
